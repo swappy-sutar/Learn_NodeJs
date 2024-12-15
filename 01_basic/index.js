8368176be2f5dc5e9538b984a419593e7e095b34
swapnil <sutarswapnil322@gmail.com>
2024-12-15 18:39:20 +0530

    all functions called in index.js

---
+++ 01_basic/index.js
@@ -0,0 +1,21 @@
+import Os from "./Os.js";
+
+
+// import fs from "./fs.js"
+import lodash from "./lodash.js"
+
+console.log("the uniqe data is: ", lodash.filter);
+
+ 
+
+// console.log(age);
+
+
+
+import notes from "./ageCalculate.js";
+const age = notes.ageCal(2001);
+
+
+import JSON from "./JSON.js";
+console.log(JSON.data);
+console.log("Convert string to object",JSON.dataObject);
